0ce855200956be5cb0d8fa9bd2c33fa849027fc9
felipegiacomoni <felipe00510@gmail.com>
2022-09-29 18:28:13 -0300

    Adding initial filter

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,14 @@ import starWars from "../apis/starWars";
 export const fetchResults = category => {
     return async dispatch => {
         const response = await starWars.get(`/${category}`);
-        console.log(response.data.results)
         dispatch({type:'FETCH_RESULTS', payload: response.data.results});
+    }
+}
+
+export const filterResults = filter => {
+    return (dispatch, getState) => {
+        const filteredResults = getState().results.filter(elem => elem.name === filter);
+        console.log(filteredResults);
+        dispatch({type: 'FETCH_RESULTS', payload: filteredResults})
     }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryButton from "./CategoryButton";
-import '../styles/SearchBar.css'
+import '../styles/SearchBar.css';
+import { connect } from "react-redux";
+import { filterResults } from "../actions";
 
-const SearchBar = () => {
+const SearchBar = props => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     const categories = [
         {label: 'Films', param: 'films'},
         {label: 'People', param: 'people'},
@@ -20,18 +24,27 @@ const SearchBar = () => {
         )
     }
 
+    const onFormSubmit = event => {
+        event.preventDefault();
+        props.filterResults(searchTerm);
+    }
+
+    const onInputChange = event => {
+        setSearchTerm(event.target.value);
+    }
+
     return (
-        <div className="ui segment search-bar search-bar-background">
-            <form className="ui form">
+        <div className="ui segment search-bar search-bar-background ">
+            <form className="ui form form-search-bar" onSubmit={onFormSubmit}>
                 <div className="field div-search-bar">
-                    <input type="text" placeholder="Search..." className="input-search-bar"/>
-                    <div className="ui buttons div-category-select">
-                        {getButtons()}
-                    </div>
+                    <input type="text" placeholder="Search..." value={searchTerm} onChange={onInputChange} className="input-search-bar"/>
                 </div>
             </form>
+            <div className="ui buttons div-category-select">
+                {getButtons()}
+            </div>
         </div>
     )
 }
 
-export default SearchBar;
+export default connect(null, {filterResults})(SearchBar);
