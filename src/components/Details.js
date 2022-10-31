59cd782bdaabcdbc4f2e382e8210cb4b93fce334
felipegiacomoni <felipe00510@gmail.com>
2022-10-31 00:47:15 -0300

    remove unused var from mapstatetoprops

--- src/components/Details.js
+++ src/components/Details.js
@@ -81,7 +81,7 @@ const Details = props => {
 }
 
 const mapStateToProps = state => {
-    return { details: state.details, category: state.search.category }
+    return { category: state.search.category }
 }
 
 export default connect(mapStateToProps)(Details);
